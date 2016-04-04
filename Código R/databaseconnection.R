2311cdae8c57d340ea8e211917b4759d1b1cc8a0
Sandy Porto <sandyporto@gmail.com>
2016-04-04 10:26:01 -0300

    Conexão ao Banco
    Códigos em R para conetar com o banco

---
+++ Código R/databaseconnection.R
@@ -0,0 +1,19 @@
+setwd("C:/Users/LAB/Desktop/Código R")
+library(RODBC)
+
+banco = "MySQL MT5"
+login = "mt5"
+senha = "mt5mt5mt5"
+
+ch = odbcConnect(banco,login,senha)
+
+query = "select * from doleta"
+
+dados = sqlQuery(ch,query)
+
+print(dados[nrow(dados)*0.1,])
+
+
+
+
+odbcClose(ch)
