70e399df92f042fee24d68977062c531c7488664
Sandy Porto <sandyporto@gmail.com>
2016-06-10 08:54:23 -0300

    Configuração final dos testes
    Para os dados antigos.

--- CodeR/testForecast.R
+++ CodeR/testForecast.R
@@ -20,12 +20,12 @@ variacaoalta = c(0.1,+Inf)
 
 
 typelist = c(open, close, high, low)
-minuteslist = c(1,5,15,30,60)
-blocklist = c(20,60,100,140)
+minuteslist = c(1)
+blocklist = c(20)
 omega = 1
 mi = 0.2
 pteste = 0.05
-rangelist = c(500,1000,2000)
+rangelist = c(1000)
 ntests = 10
 
 nrows = length(typelist)*length(minuteslist)*length(blocklist)*length(rangelist)
